25539ed495f5bd4f15ee37688f285b39a2afe020
kashtanov.aleksandr <kashtanov.aleksandr@fibertrade.ru>
2021-01-27 18:03:41 +0700

    init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SpeechRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(66, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextChanged += new System.EventHandler(this.label1_Click);
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 88);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(145, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 88);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(275, 162);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 88);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 262);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Speech.Recognition;
+
+namespace SpeechRecognition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static Label l;
+
+        static void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result.Confidence > 0.7) l.Text = e.Result.Text;
+        }	
+        
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            l = label1;
+            
+            System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("en-EN");
+            SpeechRecognitionEngine sre = new SpeechRecognitionEngine(ci);
+            sre.SetInputToDefaultAudioDevice();
+          
+            sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecognized);
+          
+
+            Choices numbers = new Choices();
+            numbers.Add(new string[] { "one", "two", "three", "alexandr", "Vlad eats banana", "misha"});
+
+   
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Culture = ci;
+            gb.Append(numbers);
+
+
+            Grammar g = new Grammar(gb);
+            sre.LoadGrammar(g);
+
+            sre.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "three")
+            {
+                if(button3.BackColor==Color.Green)
+                button3.BackColor = Color.Red;
+                else
+                button3.BackColor = Color.Green;
+            }
+            if (label1.Text == "two")
+            {
+                if (button2.BackColor == Color.Green)
+                    button2.BackColor = Color.Red;
+                else
+                    button2.BackColor = Color.Green;
+            }
+            if (label1.Text == "one")
+            {
+                if (button1.BackColor == Color.Green)
+                    button1.BackColor = Color.Red;
+                else
+                    button1.BackColor = Color.Green;
+            }
+
+        }
+    }
+}
